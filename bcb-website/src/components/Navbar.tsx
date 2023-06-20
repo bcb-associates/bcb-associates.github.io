dbb67dfc904fccbdd767863a1798774c9df9bc74
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-20 11:33:20 +0600

    added the navbar and footer

--- bcb-website/src/components/Navbar.tsx
+++ bcb-website/src/components/Navbar.tsx
@@ -1,8 +1,40 @@
+import BcbLogo from "../assets/bcbLogo.png";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <>
-      <div>Navbar</div>
-    </>
+    <nav>
+      {/* holds the 2 parts */}
+      <div className="flex items-end">
+        {/* images and slogans */}
+        <div className="basis-2/3 pl-80">
+          <img className="" src={BcbLogo}></img>
+          <h2 className="text-2xl">Total VAT & Tax Solution Center</h2>
+        </div>
+        {/* links */}
+        <div className="ml-16 basis-1/3">
+          <ul className="flex gap-x-2">
+            <li>
+              <Link
+                to="/companies"
+                className="text-xl font-bold italic text-violet-500 hover:text-2xl hover:underline hover:transition-all"
+              >
+                /companies
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/dashboard"
+                className="text-xl font-bold italic text-violet-500 hover:text-2xl hover:underline hover:transition-all"
+              >
+                /dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className="mx-auto mb-5 w-2/3 border-2 border-green-500" />
+    </nav>
   );
 };
 
--- bcb-website/src/main.tsx
+++ bcb-website/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Navbar />
     <App />
-  </React.StrictMode>,
-)
+    <Footer />
+  </React.StrictMode>
+);
